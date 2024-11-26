72fcddd56bfc2d1ddf0f4ec947ef6577491b40ed
Cal <lordcalwong@gmail.com>
2024-11-25 16:45:35 -0800

    20241125 experiment
    20241125 experiment

--- TekCapture.py
+++ TekCapture.py
@@ -1,25 +1,39 @@
 # Connect to scope to set up, trigger, and save image.
 
-import pyvisa_py as visa
+# Select the PyVISA-py backend
+import pyvisa 
+rm = pyvisa.ResourceManager('@py')
+rm.list_resources()
+
 from tm_devices import DeviceManager
 from tm_devices.drivers import MSO5B
-with DeviceManager(verbose=False) as device_manager:
-    scope :MSO5B= device_manager.add_scope("10.101.100.236")
+
+# # import time
+# # import numpy
+
 # Modify the following lines to configure this script for your instrument
 #==============================================
+visaResourceAddr = '10.101.100.236'
 #visaResourceAddr = 'TCPIP::10.101.100.236::INSTR'
 #fileSaveLocation = 'C:\Users\Calvert.Wong\OneDrive - qsc.com\Desktop\'
 #==============================================
 
+with DeviceManager(verbose=False) as device_manager:
+    scope:MSO5B = device_manager.add_scope(visaResourceAddr)
+    print()
+    print(scope.idn_string)
+
+
+
 # rm = visa.ResourceManager()
-scope.query("*IDN?")
+# scope.query("*IDN?")
 # scope = rm.open_resource(visaResourceAddr)
 
 # print(scope.query('*IDN?'))
 
-scope.commands.display.select.source.write("CH2")
+# scope.commands.display.select.source.write("CH2")
 
-scope.commands.trigger.a.type.write("EDGE")
+# scope.commands.trigger.a.type.write("EDGE")
 
 
 # scope.write("SAVe:IMAGe:FILEFormat PNG")
